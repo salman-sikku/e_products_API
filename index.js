8522b62419bf8f6f1b1bf09381250386c5fb718f
Salman <salman1995370@gmail.com>
2023-02-22 07:01:01 -0500

    three

--- index.js
+++ index.js
@@ -3,16 +3,17 @@ const PORT = 3030;
 const cors = require("cors");
 const connectedDB = require('./db/dbconnet')
 const app = express();
+
 connectedDB();
 app.use(cors());
 
-const router_api = require('./routes/prodects')
-app.use('/api/products' , router_api);
+
+app.use(require("./routes/prodects"));
 
 app.get("/", (req, res) => {
-  res.status(200).send(
-    '<h2> Hi , I am live</h2>'
-  );
+  res.status(200).send({
+    msg: "Hi , I am on Live",
+  });
 });
 
 app.listen(PORT, console.log(`I am connnet on ${PORT}`));
